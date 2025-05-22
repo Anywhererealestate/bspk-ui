1c13382d08c01f392d8bd628cf4f2f1ef3ccdb6f
anybrian <brian.reed@anywhere.re>
2025-05-21 22:47:47 -0400

    fix: publish meta generator script

--- .scripts/test-publish.sh
+++ .scripts/test-publish.sh
@@ -2,4 +2,4 @@
 # it will build the package, create a tarball, extract it to a directory and link it.
 # in the demo app run `npm link @bspk/ui` then `npm run preview`
 
-npm run build && npm pack && tar -xf bspk-ui*.tgz package && cd package && npm link
+npm run build && npm pack && tar -xf bspk-ui*.tgz package && cd package && npm link && cd .. 
