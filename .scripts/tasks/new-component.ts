16aa904cd2e4e3c3ac040502d09129c32483989e
anybrian <brian.reed@anywhere.re>
2025-06-10 12:15:43 -0400

    add new component generator

---
+++ .scripts/tasks/new-component.ts
@@ -0,0 +1,80 @@
+/* eslint-disable no-console */
+/**
+ * This script generates a new component with all the expected boilerplate.
+ *
+ * $ npx tsx .scripts/tasks/new-component.ts ${componentName}
+ */
+
+import fs from 'fs';
+import path from 'path';
+
+import { componentsDir, kebabCase } from '../utils';
+
+const componentName = process.argv[2];
+
+if (!componentName) {
+    console.error('Please provide a component name.');
+    process.exit(1);
+}
+
+const componentFilePath = path.join(componentsDir, `${componentName}.tsx`);
+
+if (fs.existsSync(componentFilePath)) {
+    console.error(`Component ${componentName} already exists.`);
+    process.exit(1);
+}
+
+const slug = kebabCase(componentName);
+
+fs.writeFileSync(
+    componentFilePath,
+
+    `import { ReactNode } from 'react';
+import './${slug}.scss';
+
+const DEFAULT = {
+    variant: 'none',
+} as const;
+
+export type ${componentName}Props = {
+    /**
+     * The content of the ${slug}.
+     *
+     * @required
+     */
+    children: ReactNode;
+    /**
+     * The variant of the ${slug}.
+     *
+     * @default none
+     */
+    variant?: 'none';
+};
+
+/**
+ * Component description.
+ *
+ * @example
+ *     import { ${componentName} } from '@bspk/ui/${componentName}';
+ *
+ *     function Example() {
+ *         return <${componentName}>Example ${componentName}</${componentName}>;
+ *     }
+ *
+ * @name ${componentName}
+ */
+function ${componentName}({ children, variant = DEFAULT.variant }: ${componentName}Props) {
+    return (
+        <span data-bspk="${slug}" data-variant={variant || undefined}>
+            {children}
+        </span>
+    );
+}
+
+${componentName}.bspkName = '${componentName}';
+
+export { ${componentName} };
+
+/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
+`,
+);
