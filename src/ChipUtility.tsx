58b08730056a367aeed9d834ee464ef9dbf51d7c
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-06-23 22:14:11 -0500

    feat: add ChipUtility component
    add console logs
    
    ChipUtility accessibility and conditionals added
    
    ChipUtility scss updates

---
+++ src/ChipUtility.tsx
@@ -0,0 +1,109 @@
+import './chip-utility.scss';
+import { ReactNode, isValidElement } from 'react';
+
+import { isValidIcon } from './utils/children';
+
+// import { CommonProps } from '.';
+
+export type ChipUtilityProps = {
+    /**
+     * Is the chip disabled.
+     *
+     * @default false
+     */
+    disabled?: boolean;
+
+    /**
+     * Is the chip elevated or flat. If flat the drop shadow is removed.
+     *
+     * @default false
+     */
+    flat?: boolean;
+
+    /**
+     * The label of the chip.
+     *
+     * @example
+     *     Chip Label;
+     */
+    label?: string;
+
+    /** The function to call when the chip is clicked. */
+    onClick?: (event: React.MouseEvent<HTMLSpanElement, MouseEvent>) => void;
+
+    /**
+     * Whether the chip is currently selected.
+     *
+     * @default false
+     */
+    selected?: boolean;
+
+    /**
+     * The leading icon of the chip.
+     *
+     * @type BspkIcon
+     */
+    leadingIcon?: ReactNode;
+
+    /**
+     * The trailing icon of the chip.
+     *
+     * @type BspkIcon
+     */
+    trailingIcon?: ReactNode;
+};
+
+/**
+ * Dynamically generated options that are suggested to the user as responses or prompts.
+ *
+ * @example
+ *     import { ChipUtility } from '@bspk/ui/ChipUtility';
+ *
+ *     function Example() {
+ *         return (
+ *             <Chip label="Label" onClick={() => console.log('Chip clicked!')}>
+ *                 Example Chip
+ *             </Chip>
+ *         );
+ *     }
+ *
+ * @name ChipUtility
+ * @phase WorkInProgress
+ */
+function ChipUtility({
+    flat = false,
+    disabled = false,
+    label,
+    selected = false,
+    leadingIcon,
+    onClick,
+    trailingIcon,
+}: ChipUtilityProps) {
+    return (
+        <button
+            data-bspk="chip-utility"
+            data-disabled={disabled || undefined}
+            data-flat={flat || undefined}
+            data-selected={selected || undefined}
+            onClick={disabled ? undefined : onClick}
+        >
+            {isValidIcon(leadingIcon) && isValidElement(leadingIcon) && (
+                <span aria-hidden="true" data-chip-icon>
+                    {leadingIcon}
+                </span>
+            )}
+            {label}
+            {isValidIcon(trailingIcon) && isValidElement(trailingIcon) && (
+                <span aria-hidden="true" data-chip-icon>
+                    {trailingIcon}
+                </span>
+            )}
+        </button>
+    );
+}
+
+ChipUtility.bspkName = 'ChipUtility';
+
+export { ChipUtility };
+
+/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
---
+++ src/demo/examples/ChipUtility.tsx
@@ -0,0 +1,45 @@
+import { ChipUtilityProps } from '../../ChipUtility';
+import { ComponentExample } from '../utils';
+
+export const ChipUtilityExample: ComponentExample<ChipUtilityProps> = {
+    presets: [
+        {
+            label: 'Basic',
+            propState: {
+                label: 'chip',
+            },
+        },
+        {
+            label: 'Leading Icon',
+            propState: {
+                label: 'chip',
+                leadingIcon: 'Add',
+                trailingIcon: '',
+            },
+        },
+        {
+            label: 'Trailing Icon',
+            propState: {
+                label: 'chip',
+                leadingIcon: '',
+                trailingIcon: 'Add',
+            },
+        },
+        {
+            label: 'Both Icons',
+            propState: {
+                label: 'chip',
+                leadingIcon: 'Add',
+                trailingIcon: 'Add',
+            },
+        },
+        {
+            label: 'Invalid Icons',
+            propState: {
+                label: 'chip',
+                leadingIcon: 'Bumblebee',
+                trailingIcon: 'Rabbit',
+            },
+        },
+    ],
+};
