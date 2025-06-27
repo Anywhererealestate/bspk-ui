73504e3ffc55b0d608e5115b71db192111c23b42
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-06-27 15:48:01 -0500

    feat: Add ChipGroupUtility component

---
+++ src/ChipGroupUtility.tsx
@@ -0,0 +1,64 @@
+import './chip-group-utility.scss';
+
+import { ChipUtility, ChipUtilityProps } from './ChipUtility';
+
+export type ChipGroupItem = Pick<
+    ChipUtilityProps,
+    'disabled' | 'flat' | 'label' | 'leadingIcon' | 'onClick' | 'selected' | 'trailingIcon'
+>;
+
+export type ChipGroupUtilityProps = {
+    /**
+     * The array of Chip items.
+     *
+     * @example
+     *     [
+     *         { label: 'chip 1' },
+     *         { label: 'chip 2' },
+     *         { label: 'chip 3' },
+     *         { label: 'chip 4' },
+     *         { label: 'chip 5' },
+     *         { label: 'chip 6' },
+     *         { label: 'chip 7' },
+     *     ];
+     *
+     * @type Array<ChipGroupItem>
+     * @required
+     */
+    items: ChipGroupItem[];
+    /**
+     * The variant of the chip-group-utility.
+     *
+     * @default true
+     */
+    wrap?: boolean;
+};
+
+/**
+ * Component description.
+ *
+ * @example
+ *     import { ChipGroupUtility } from '@bspk/ui/ChipGroupUtility';
+ *
+ *     function Example() {
+ *         return <ChipGroupUtility>Example ChipGroupUtility</ChipGroupUtility>;
+ *     }
+ *
+ * @name ChipGroupUtility
+ * @phase WorkInProgress
+ */
+function ChipGroupUtility({ items, wrap }: ChipGroupUtilityProps) {
+    return (
+        <span data-bspk="chip-group-utility" data-wrap={wrap || undefined}>
+            {items.map((item, idx) => (
+                <ChipUtility key={`chip-${idx}`} {...item} />
+            ))}
+        </span>
+    );
+}
+
+ChipGroupUtility.bspkName = 'ChipGroupUtility';
+
+export { ChipGroupUtility };
+
+/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
