ac4b40c9e324d8113ba7fb171ed2f9653945349f
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-07-01 11:48:05 -0500

    feat: add ChipGroupUtility (#49)
    * feat: Add ChipGroupUtility component
    
    * chore: Add trailing badge to ChipUtility
    
    * chore: Add touch target to ChipUtility
    
    * chore: update Button and ChipUtility touch target; update phase in ChipUtility, ChipGroupUtility, and ToggleOption; add overflow scroll to SegmentedControl
    
    * chore: add trailingBadge to ChipUtilityProps

---
+++ src/ChipGroupUtility.tsx
@@ -0,0 +1,77 @@
+import './chip-group-utility.scss';
+
+import { ChipUtility, ChipUtilityProps } from './ChipUtility';
+
+export type ChipGroupItem = Pick<
+    ChipUtilityProps,
+    'disabled' | 'flat' | 'label' | 'leadingIcon' | 'onClick' | 'selected' | 'trailingBadge' | 'trailingIcon'
+>;
+
+export type ChipGroupUtilityProps = {
+    /**
+     * The array of Chip items.
+     *
+     * @example
+     *     [
+     *         { label: 'chip 1' },
+     *         { label: 'chip 2' },
+     *         { label: 'chip 3' },
+     *         { label: 'chip 4' },
+     *         { label: 'chip 5' },
+     *         { label: 'chip 6' },
+     *         { label: 'chip 7' },
+     *     ];
+     *
+     * @type Array<ChipGroupItem>
+     * @required
+     */
+    items: ChipGroupItem[];
+    /**
+     * To allow chips to wrap. If set to false chips will scroll.
+     *
+     * @default true
+     */
+    wrap?: boolean;
+};
+/**
+ * Component description.
+ *
+ * @example
+ *     import { ChipGroupUtility } from '@bspk/ui/ChipGroupUtility';
+ *
+ *     function Example() {
+ *         return (
+ *             <ChipGroupUtility
+ *                 items={[
+ *                     { label: 'item 1' },
+ *                     { label: 'item 2' },
+ *                     { label: 'item 3' },
+ *                     { label: 'item 4' },
+ *                     { label: 'item 5' },
+ *                     { label: 'item 6' },
+ *                     { label: 'item 7' },
+ *                 ]}
+ *                 label="chip group example"
+ *                 wrap={true}
+ *             />
+ *         );
+ *     }
+ *
+ * @name ChipGroupUtility
+ * @phase Utility
+ */
+function ChipGroupUtility({ items, wrap = true }: ChipGroupUtilityProps) {
+    return (
+        <span data-bspk="chip-group-utility" data-wrap={wrap || undefined}>
+            {items.map((item, idx) => (
+                <ChipUtility key={`chip-${idx}`} {...item} />
+            ))}
+        </span>
+    );
+}
+
+ChipGroupUtility.bspkName = 'ChipGroupUtility';
+
+export { ChipGroupUtility };
+
+/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
--- src/ChipUtility.tsx
+++ src/ChipUtility.tsx
@@ -1,7 +1,10 @@
 import './chip-utility.scss';
 import { ReactNode, isValidElement } from 'react';
 
+import { Badge, BadgeProps } from './Badge';
 import { isValidIcon } from './utils/children';
+
+export type BadgeItem = Pick<BadgeProps, 'count' | 'size' | 'surfaceBorder' | 'variant'>;
 
 export type ChipUtilityProps = {
     /**
@@ -40,9 +43,18 @@ export type ChipUtilityProps = {
     /**
      * The trailing icon of the chip.
      *
+     * You can only have one of the trailing options, trailingIcon **or** trailingBadge. If both are present the
+     * trailingIcon will be visible.
+     *
      * @type BspkIcon
      */
     trailingIcon?: ReactNode;
+    /**
+     * The trailing Badge for use in the ChipFilter.
+     *
+     * If a trailingIcon is provided the Badge will **not** be visible.
+     */
+    trailingBadge?: BadgeItem;
 };
 
 /**
@@ -70,6 +82,7 @@ function ChipUtility({
     leadingIcon,
     onClick,
     trailingIcon,
+    trailingBadge,
 }: ChipUtilityProps) {
     return (
         <button
@@ -77,19 +90,31 @@ function ChipUtility({
             data-disabled={disabled || undefined}
             data-flat={flat || undefined}
             data-selected={selected || undefined}
+            data-touch-target-parent
             onClick={disabled ? undefined : onClick}
         >
-            {isValidIcon(leadingIcon) && isValidElement(leadingIcon) && (
-                <span aria-hidden="true" data-chip-icon>
-                    {leadingIcon}
-                </span>
-            )}
-            {label}
-            {isValidIcon(trailingIcon) && isValidElement(trailingIcon) && (
-                <span aria-hidden="true" data-chip-icon>
-                    {trailingIcon}
-                </span>
-            )}
+            <>
+                {isValidIcon(leadingIcon) && isValidElement(leadingIcon) && (
+                    <span aria-hidden="true" data-chip-icon>
+                        {leadingIcon}
+                    </span>
+                )}
+                <span>{label}</span>
+                {isValidIcon(trailingIcon) && isValidElement(trailingIcon) && (
+                    <span aria-hidden="true" data-chip-icon>
+                        {trailingIcon}
+                    </span>
+                )}
+                {trailingBadge && !trailingIcon && (
+                    <Badge
+                        count={trailingBadge.count}
+                        size={trailingBadge.size}
+                        surfaceBorder={trailingBadge.surfaceBorder}
+                        variant={trailingBadge.variant}
+                    />
+                )}
+            </>
+            <span data-touch-target />
         </button>
     );
 }
