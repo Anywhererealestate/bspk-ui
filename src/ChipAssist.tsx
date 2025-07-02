952f678d7ae837c29ed99c5f3a274cd3813b2d17
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-07-02 12:37:15 -0500

    feat: Add ChipAssist component (#52)
    * feat: Add ChipAssist component

---
+++ src/ChipAssist.tsx
@@ -0,0 +1,33 @@
+import { ChipUtility, ChipUtilityProps } from './ChipUtility';
+
+export type ChipAssistProps = Pick<ChipUtilityProps, 'label' | 'leadingIcon' | 'onClick'>;
+
+/**
+ * A dynamic action element that helps trigger and perform an action for the customer. A supplement option to buttons.
+ *
+ * @example
+ *     import { ChipAssist } from '@bspk/ui/ChipAssist';
+ *     import { SvgCloud } from '@bspk/icons/Cloud';
+ *
+ *     function Example() {
+ *         return (
+ *             <ChipAssist
+ *                 label="Assist Chip"
+ *                 leadingIcon=<SvgCloud />
+ *                 onClick={() => console.log('Assist Chip clicked')}
+ *             />
+ *         );
+ *     }
+ *
+ * @name ChipAssist
+ * @phase DesignReview
+ */
+function ChipAssist({ label, leadingIcon, onClick }: ChipAssistProps) {
+    return <ChipUtility label={label} leadingIcon={leadingIcon} onClick={onClick} />;
+}
+
+ChipAssist.bspkName = 'ChipAssist';
+
+export { ChipAssist };
+
+/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
---
+++ src/demo/examples/ChipAssist.tsx
@@ -0,0 +1,24 @@
+import { ChipAssistProps } from '../../ChipAssist';
+import { ComponentExampleFn } from '../utils';
+
+export const ChipAssistExample: ComponentExampleFn<ChipAssistProps> = ({ action }) => ({
+    containerStyle: { width: '100%' },
+    render: ({ props, Component }) => {
+        return <Component {...props} onClick={() => action('ChipAssist clicked!')} />;
+    },
+    presets: [
+        {
+            label: 'Basic ChipAssist',
+            propState: {
+                label: 'chip option',
+            },
+        },
+        {
+            label: 'with leadingIcon',
+            propState: {
+                label: 'chip option',
+                leadingIcon: 'SignLanguage',
+            },
+        },
+    ],
+});
