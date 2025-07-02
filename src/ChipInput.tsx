6e312e01ba1a124e7e0b95a7ac152d898381c061
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-07-02 12:36:50 -0500

    feat: Add ChipInput component (#54)
    * feat: Add ChipInput component

---
+++ src/ChipInput.tsx
@@ -0,0 +1,49 @@
+import { SvgClose } from '@bspk/icons/Close';
+
+import { ChipUtility, ChipUtilityProps } from './ChipUtility';
+
+export type ChipInputProps = Pick<ChipUtilityProps, 'label' | 'leadingIcon' | 'onClick'> & {
+    /**
+     * If true, a close icon will be added and the chip can be removed.
+     *
+     * @default true
+     */
+    removable?: boolean;
+};
+
+/**
+ * A range of short form key words or pieces of information a customer enters within multi entry field.
+ *
+ * @example
+ *     import { ChipInput } from '@bspk/ui/ChipInput';
+ *     import { SvgCloud } from '@bspk/icons/Cloud';
+ *
+ *     function Example() {
+ *         return (
+ *             <ChipInput
+ *                 label="InputChip"
+ *                 leadingIcon=<SvgCloud />
+ *                 onClick={() => console.log('Input Chip clicked')}
+ *             />
+ *         );
+ *     }
+ *
+ * @name ChipInput
+ * @phase DesignReview
+ */
+function ChipInput({ label, leadingIcon, onClick, removable = true }: ChipInputProps) {
+    return (
+        <ChipUtility
+            label={label}
+            leadingIcon={leadingIcon}
+            onClick={onClick}
+            trailingIcon={removable ? <SvgClose /> : undefined}
+        />
+    );
+}
+
+ChipInput.bspkName = 'ChipInput';
+
+export { ChipInput };
+
+/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
---
+++ src/demo/examples/ChipInput.tsx
@@ -0,0 +1,39 @@
+import { ChipInputProps } from '../../ChipInput';
+import { ComponentExampleFn } from '../utils';
+
+export const ChipInputExample: ComponentExampleFn<ChipInputProps> = ({ action }) => ({
+    containerStyle: { width: '100%' },
+    render: ({ props, Component }) => {
+        return <Component {...props} onClick={() => action('ChipInput clicked!')} />;
+    },
+    presets: [
+        {
+            label: 'Basic ChipInput',
+            propState: {
+                label: 'chip option',
+            },
+        },
+        {
+            label: 'with leadingIcon',
+            propState: {
+                label: 'chip option',
+                leadingIcon: 'SignLanguage',
+            },
+        },
+        {
+            label: 'with removable = false',
+            propState: {
+                label: 'chip option',
+                removable: false,
+            },
+        },
+        {
+            label: 'with removable = false and leadingIcon',
+            propState: {
+                label: 'chip option',
+                leadingIcon: 'SignLanguage',
+                removable: false,
+            },
+        },
+    ],
+});
