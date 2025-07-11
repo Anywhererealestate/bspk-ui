9b5bdf5dd19a3917a527c249230e58b999422b70
anybrian <brian.reed@anywhere.re>
2025-07-10 21:34:14 -0400

    add styles-provider-demo, update css fix

---
+++ src/components/StylesProviderDemo/StylesProviderDemo.tsx
@@ -0,0 +1,55 @@
+import { useRef, useEffect } from 'react';
+import { BRANDS_CSS } from './brandsCss';
+import { Brand } from '-/types/common';
+import '-/styles/base.scss';
+
+export type StylesProviderDemoProps = {
+    /**
+     * The current brand variant.
+     *
+     * @default anywhere
+     */
+    brand?: Brand;
+};
+
+/**
+ * Used only on the demo site to switch between different brands styles.
+ *
+ * @example
+ *     import { StylesProviderDemo } from '@bspk/ui/StylesProviderDemo';
+ *
+ *     function Example() {
+ *         return <StylesProviderDemo brand="anywhere" />;
+ *     }
+ *
+ * @name StylesProviderDemo
+ * @phase Utility
+ */
+function StylesProviderDemo({ brand }: StylesProviderDemoProps): JSX.Element | null {
+    const styleElement = useRef<HTMLStyleElement | null>(null);
+
+    useEffect(() => {
+        if (!styleElement.current) {
+            styleElement.current = document.createElement('style');
+            document.head.appendChild(styleElement.current);
+        }
+
+        if (styleElement.current) {
+            styleElement.current.textContent = (brand && BRANDS_CSS[brand]) || BRANDS_CSS.anywhere;
+        }
+        return () => {
+            if (styleElement.current) {
+                document.head.removeChild(styleElement.current);
+                styleElement.current = null;
+            }
+        };
+    }, [brand]);
+
+    return null;
+}
+
+StylesProviderDemo.bspkName = 'StylesProviderDemo';
+
+export { StylesProviderDemo };
+
+/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
