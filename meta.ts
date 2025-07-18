381e89d8d1d17b40e2ea132700728b7ef1c12756
anybrian <brian.reed@anywhere.re>
2025-07-18 16:14:46 -0400

    add test

--- meta.ts
+++ meta.ts
@@ -546,7 +546,7 @@ async function createMeta() {
             `export const componentsMeta = meta.componentsMeta as ComponentMeta[];
 export const utilitiesMeta = meta.utilitiesMeta as UtilityMeta[];
 export const typesMeta = meta.typesMeta as TypeMeta[];
-export const MODE = meta.MODE as 'development' | 'production';
+export const MODE = meta.MODE as 'development' | 'production' | 'test';
 export const UI_HASH = meta.UI_HASH as string;
 export const VERSION = meta.VERSION as string;
 export const BUILD = meta.BUILD as string;`,
