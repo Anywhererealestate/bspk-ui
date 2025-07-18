4fc0f0980bc1ad1f4c955490b5ec17266bdf8117
anybrian <brian.reed@anywhere.re>
2025-07-18 17:52:34 -0400

    feat: first pass on time picker, adds bound (#78)
    * wip
    
    * wip
    
    * feat: first pass on time picker, adds bound

---
+++ src/components/TimeInput/TimeInputExample.tsx
@@ -0,0 +1,6 @@
+import { TimeInputProps } from './TimeInput';
+import { ComponentExample } from '-/utils/demo';
+
+export const TimeInputExample: ComponentExample<TimeInputProps> = {
+    hideVariants: true,
+};
