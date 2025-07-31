1e515b8c91cc4b1825bd330e5c439c2c1f83c3a2
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-07-31 13:02:51 -0500

    chore(Rating): update phase to UXReview

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -47,7 +47,7 @@ export type RatingProps = {
  *     }
  *
  * @name Rating
- * @phase QA
+ * @phase UXReview
  */
 function Rating({ size = 'medium', value, onChange }: RatingProps) {
     const As: ElementType = onChange ? 'button' : 'div';
