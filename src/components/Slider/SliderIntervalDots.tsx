90e088eda40025739bcaed2b470fe9a3997a87f0
emileanywhere <emile.fleming@anywhere.re>
2025-07-31 10:31:59 -0400

    Update src/components/Slider/SliderIntervalDots.tsx
    Co-authored-by: anybrian <brian.reed@anywhere.re>

--- src/components/Slider/SliderIntervalDots.tsx
+++ src/components/Slider/SliderIntervalDots.tsx
@@ -7,21 +7,19 @@ type IntervalDotProps = Pick<SliderProps, 'maximum' | 'minimum' | 'value'> & {
 export function SliderIntervalDots({ interval, maximum, minimum, value }: IntervalDotProps) {
     if (interval <= 0) return null;
 
-    const count = Math.floor((maximum - minimum) / interval);
-    const dots = [];
 
-    for (let i = 1; i < count; i++) {
+const length = Math.floor((maximum - minimum) / interval);
+
+   return const newArray = Array.from({ length: n }, (_, i) => {
         const pointValue = i * interval;
         const percent = (pointValue / (maximum - minimum)) * 100;
-        dots.push(
-            <div
-                data-filled-section={pointValue < value ? '' : undefined}
-                data-interval-dot
+        
+  return <div
+                data-interval-dot={pointValue < value ? 'filled' : ''}
                 key={i}
                 style={{ left: `${percent}%` }}
-            />,
-        );
-    }
+            />;
+});
 
     return dots;
 }
