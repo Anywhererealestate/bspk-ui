d29ec3291b51f3e9838f6fe65362ea8b61081e5d
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-08-05 18:40:03 -0500

    PhoneNumberInput passes axe DevTools

--- src/components/Listbox/Listbox.tsx
+++ src/components/Listbox/Listbox.tsx
@@ -169,7 +169,7 @@ function Listbox<Item extends ListboxItemProps>({
                     onClick={(event: React.MouseEvent<HTMLElement, MouseEvent>) => {
                         onChange?.(allSelected ? [] : items.map((item) => item.value), event);
                     }}
-                    tabIndex={-1}
+                    role="option"
                     trailing={
                         <Checkbox
                             aria-label={selectAll}
@@ -200,7 +200,6 @@ function Listbox<Item extends ListboxItemProps>({
                         active={activeIndex === index || undefined}
                         aria-disabled={item.disabled || undefined}
                         aria-selected={selected || undefined}
-                        as="button"
                         disabled={item.disabled || undefined}
                         id={`${menuId}-item-${index}`}
                         key={`${menuId}-item-${index}`}
@@ -219,7 +218,7 @@ function Listbox<Item extends ListboxItemProps>({
                                 );
                             }
                         }}
-                        tabIndex={-1}
+                        role="option"
                         trailing={
                             isMulti ? (
                                 <Checkbox
