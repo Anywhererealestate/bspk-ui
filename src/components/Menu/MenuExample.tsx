738f54c0a67af1a60580d97691d237beb0dee4e8
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-08-05 17:35:04 -0500

    Menu passes axe DevTools

--- src/components/Menu/MenuExample.tsx
+++ src/components/Menu/MenuExample.tsx
@@ -22,7 +22,6 @@ export const MenuExample: ComponentExampleFn<ElementProps<MenuProps, 'div'> & { 
         return (
             <Component
                 {...props}
-                aria-label="Settings Menu"
                 floating={false}
                 portal={false}
                 style={{ padding: 'var(--spacing-sizing-02) var(--spacing-sizing-02)' }}
@@ -38,10 +37,6 @@ export const MenuExample: ComponentExampleFn<ElementProps<MenuProps, 'div'> & { 
                 <ListItem
                     label="Dark mode"
                     leading={props['data-dark-mode'] ? <SvgDarkModeFill /> : <SvgDarkMode />}
-                    onClick={() => {
-                        setState((prev) => ({ 'data-dark-mode': !prev['data-dark-mode'] }));
-                        action('Dark mode toggled');
-                    }}
                     trailing={
                         <Switch
                             aria-label="Toggle dark mode"
@@ -51,7 +46,6 @@ export const MenuExample: ComponentExampleFn<ElementProps<MenuProps, 'div'> & { 
                                 setState((prev) => ({ 'data-dark-mode': !prev['data-dark-mode'] }));
                                 action('Dark mode toggled');
                             }}
-                            readOnly={true}
                         />
                     }
                 />
@@ -66,6 +60,7 @@ export const MenuExample: ComponentExampleFn<ElementProps<MenuProps, 'div'> & { 
                     onClick={() => {
                         action('Log out clicked');
                     }}
+                    role="button"
                 />
             </Component>
         );
