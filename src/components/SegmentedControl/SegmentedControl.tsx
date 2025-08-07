fa8a6fce3b1a1fd90b292436e66f3e192ab440aa
anybrian <brian.reed@anywhere.re>
2025-08-07 11:39:39 -0400

    fix: tablist - create common utlity for tabgroup and segmentedcontrol (#130)
    * fix: tablist - create common utlity for tabgroup and segmentedcontrol
    
    * clean up examples, fix icon sizes
    
    * cleanup
    
    * cleanup, trail
    
    * feedback

--- src/components/SegmentedControl/SegmentedControl.tsx
+++ src/components/SegmentedControl/SegmentedControl.tsx
@@ -1,91 +1,12 @@
-import { Fragment } from 'react';
-
-import { Tooltip } from '-/components/Tooltip';
+import { TabOption, TabList, TabListProps } from '-/components/TabList';
 import { useOptionIconsInvalid } from '-/hooks/useOptionIconsInvalid';
 import { ElementProps } from '-/types/common';
 
 import './segmented-control.scss';
 
-export type SegmentedControlOption = {
-    /**
-     * The label of the option. This is the text that will be displayed on the option.
-     *
-     * @required
-     */
-    label: string;
-    /**
-     * Determines if the element is [disabled](https://developer.mozilla.org/en-US/docs/Web/HTML/Attributes/disabled).
-     *
-     * @default false
-     */
-    disabled?: boolean;
-    /** The value of the option. If not provided, the label will be used as the value. */
-    value?: string;
-    /**
-     * The the icon to display before the label.
-     *
-     * @type BspkIcon
-     */
-    icon?: React.ReactNode;
-    /**
-     * The icon to display before the label when the option is active.
-     *
-     * @type BspkIcon
-     */
-    iconActive?: React.ReactNode;
-};
+export type SegmentedControlOption = Pick<TabOption, 'disabled' | 'icon' | 'iconSelected' | 'label' | 'value'>;
 
-export type SegmentedControlProps = {
-    /**
-     * The options to display. Each option has a label and an optional leading icon.
-     *
-     * @example
-     *     [
-     *         { value: '1', label: 'Option 1' },
-     *         { value: '2', label: 'Option 2' },
-     *         { value: '3', label: 'Option 3' },
-     *     ];
-     *
-     * @type Array<SegmentedControlOption>
-     * @required
-     */
-    options: SegmentedControlOption[];
-    /**
-     * The id of the selected option.
-     *
-     * @example
-     *     1;
-     *
-     * @required
-     */
-    value: SegmentedControlOption['value'];
-    /**
-     * The function to call when the option is clicked.
-     *
-     * @required
-     */
-    onChange: (value: SegmentedControlOption['value']) => void;
-    /**
-     * The size of the options.
-     *
-     * @default medium
-     */
-    size?: 'medium' | 'small';
-    /**
-     * The width of the options. If set to 'fill', the options will fill the width of the container. If set to 'hug',
-     * the options will be as wide as their content.
-     *
-     * @default hug
-     */
-    width?: 'fill' | 'hug';
-    /**
-     * Determines if the labels of the options should be displayed. If icons are not provided for every option this is
-     * ignored and labels are shown.
-     *
-     * @default false
-     */
-    iconsOnly?: boolean;
-};
+export type SegmentedControlProps = TabListProps<SegmentedControlOption>;
 
 /**
  * Navigation tool that organizes content across different screens and views.
@@ -121,7 +42,7 @@ function SegmentedControl({
     width = 'hug',
     iconsOnly: iconsOnlyProp = false,
     ...containerProps
-}: ElementProps<SegmentedControlProps, 'div'>) {
+}: ElementProps<SegmentedControlProps, 'ul'>) {
     const options = Array.isArray(optionsProp) ? optionsProp : [];
     useOptionIconsInvalid(options);
 
@@ -129,32 +50,16 @@ function SegmentedControl({
     const iconsOnly = iconsOnlyProp === true && options.every((item) => item.icon && item.label);
 
     return (
-        <div {...containerProps} data-bspk="segmented-control" data-size={size} data-width={width}>
-            {options.map((item, index) => {
-                const isActive = item.value === value;
-                return (
-                    <Fragment key={item.value}>
-                        <Tooltip disabled={!iconsOnly} label={item.label} placement="top">
-                            <button
-                                aria-label={item.label}
-                                data-first={index === 0 || undefined}
-                                data-last={index === options.length - 1 || undefined}
-                                data-selected={isActive || undefined}
-                                disabled={item.disabled || undefined}
-                                onClick={() => onChange(item.value || item.label)}
-                            >
-                                <span data-outer>
-                                    <span data-inner>
-                                        {(isActive && item.iconActive) || item.icon}
-                                        {!iconsOnly && item.label}
-                                    </span>
-                                </span>
-                            </button>
-                        </Tooltip>
-                    </Fragment>
-                );
-            })}
-        </div>
+        <TabList
+            {...containerProps}
+            data-bspk="segmented-control"
+            iconsOnly={iconsOnly}
+            onChange={onChange}
+            options={options}
+            size={size}
+            value={value}
+            width={width}
+        />
     );
 }
 
---
+++ src/hooks/useKeyNavigation.ts
@@ -0,0 +1,66 @@
+import { Dispatch, DOMAttributes, SetStateAction, useRef, useState } from 'react';
+import { useOutsideClick } from './useOutsideClick';
+import { handleKeyDown } from '-/utils/handleKeyDown';
+
+export type UseKeyNavigationProps = {
+    /** The callback function to call when a tab is selected (Enter or Space keys). */
+    onSelect: (activeId?: string) => void;
+};
+
+/**
+ * Custom hook to handle keyboard navigation for a list of elements.
+ *
+ * This hook provides functionality to navigate through elements using arrow keys and select an element with the Enter
+ * or Space key, or onClick.
+ */
+export function useKeyNavigation<T extends HTMLElement>({
+    onSelect,
+}: UseKeyNavigationProps): {
+    handleKeyDown: DOMAttributes<T>['onKeyDown'];
+    activeElementId: string | null;
+    setElements: (newElements: HTMLElement[]) => void;
+    setActiveElementId: Dispatch<SetStateAction<string | null>>;
+} {
+    const elements = useRef<HTMLElement[]>([]);
+    const setElements = (newElements: HTMLElement[]) => {
+        elements.current = newElements;
+    };
+
+    const [activeElementId, setActiveElementId] = useState<string | null>(null);
+
+    useOutsideClick({
+        elements: elements.current,
+        callback: () => setActiveElementId(null),
+    });
+
+    const handleIncrement = (increment: -1 | 1) => (): void => {
+        let currentElement = elements.current[0];
+        if (activeElementId)
+            currentElement = elements.current.find((el) => el.id === activeElementId) || elements.current[0];
+
+        const activeIndex = elements.current.indexOf(currentElement);
+        const nextindex = (activeIndex + increment + elements.current.length) % elements.current.length;
+        setActiveElementId(elements.current[nextindex].id);
+    };
+
+    const handleSelect = () => () => {
+        if (activeElementId) onSelect(activeElementId);
+    };
+
+    return {
+        setElements,
+        handleKeyDown: handleKeyDown(
+            {
+                ArrowRight: handleIncrement(1),
+                ArrowDown: handleIncrement(1),
+                ArrowUp: handleIncrement(-1),
+                ArrowLeft: handleIncrement(-1),
+                Enter: handleSelect(),
+                Space: handleSelect(),
+            },
+            { preventDefault: true, stopPropagation: true },
+        ),
+        activeElementId,
+        setActiveElementId,
+    };
+}
