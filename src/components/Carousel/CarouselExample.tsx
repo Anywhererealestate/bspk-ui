04a870ffdf20cbb2461d6966486a98c2e6ccbae2
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-09-08 15:29:36 -0500

    chore(Carousel): move styles to scss, add BSPK icons to control buttons

--- src/components/Carousel/CarouselExample.tsx
+++ src/components/Carousel/CarouselExample.tsx
@@ -2,17 +2,67 @@ import { CarouselProps } from '.';
 import { ComponentExample } from '-/utils/demo';
 
 export const CarouselExample: ComponentExample<CarouselProps> = {
-    containerStyle: { width: '100%' },
+    // containerStyle: { width: '100%' },
     defaultState: {},
     disableProps: [],
     presets: [],
     render: ({ props, Component }) => (
         <Component {...props}>
-            <div style={{ padding: 20, textAlign: 'center' }}>Slide 1</div>
-            <div style={{ padding: 20, textAlign: 'center' }}>Slide 2</div>
-            <div style={{ padding: 20, textAlign: 'center' }}>Slide 3</div>
-            <div style={{ padding: 20, textAlign: 'center' }}>Slide 4</div>
-            <div style={{ padding: 20, textAlign: 'center' }}>Slide 5</div>
+            <div
+                style={{
+                    background: '#F6DBFA',
+                    borderRadius: '8px',
+                    minHeight: '180px',
+                    padding: 20,
+                    textAlign: 'center',
+                }}
+            >
+                Slide 1
+            </div>
+            <div
+                style={{
+                    background: '#E7D9FC',
+                    borderRadius: '8px',
+                    minHeight: '180px',
+                    padding: 20,
+                    textAlign: 'center',
+                }}
+            >
+                Slide 2
+            </div>
+            <div
+                style={{
+                    background: '#DFE9FD',
+                    borderRadius: '8px',
+                    minHeight: '180px',
+                    padding: 20,
+                    textAlign: 'center',
+                }}
+            >
+                Slide 3
+            </div>
+            <div
+                style={{
+                    background: '#D9ECEB',
+                    borderRadius: '8px',
+                    minHeight: '180px',
+                    padding: 20,
+                    textAlign: 'center',
+                }}
+            >
+                Slide 4
+            </div>
+            <div
+                style={{
+                    background: '#FEF5E0',
+                    borderRadius: '8px',
+                    minHeight: '180px',
+                    padding: 20,
+                    textAlign: 'center',
+                }}
+            >
+                Slide 5
+            </div>
         </Component>
     ),
     sections: [],
