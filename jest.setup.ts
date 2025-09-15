20801ae32ce40a27456dedf071fd31a9d6b0993a
anybrian <brian.reed@anywhere.re>
2025-09-15 17:12:09 -0400

    cleanup?

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,7 +1,8 @@
 import '@testing-library/jest-dom';
+import { cleanup } from '@testing-library/react';
 import jestAxe from 'jest-axe';
 
 const { toHaveNoViolations } = jestAxe;
 expect.extend(toHaveNoViolations);
 
-jest.useFakeTimers();
+afterEach(cleanup);
