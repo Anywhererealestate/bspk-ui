e57e101e597f1432f82bd0e01f7b6136db925297
emileanywhere <emile.fleming@anywhere.re>
2025-09-17 09:59:45 -0400

    chore: scripts refactor (#184)
    Co-authored-by: anybrian <brian.reed@anywhere.re>

---
+++ .scripts/lib/generateStylesFile.ts
@@ -0,0 +1,22 @@
+import fs from 'fs/promises';
+import { kebabCase } from '../utils';
+
+/** Generates and writes a boilerplate component styles (sass) file for a component name and destination file path. */
+export const generateAndWriteStylesFile = async (componentName: string, filePath: string) => {
+    const styleFileContent = generateStylesFile(componentName);
+    await fs.writeFile(filePath, styleFileContent);
+};
+
+export const generateStylesFile = (componentName: string) => {
+    const slug = kebabCase(componentName);
+
+    return `[data-bspk='${slug}'] {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        width: fit-content;
+    }
+    
+    /** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
+    `;
+};
