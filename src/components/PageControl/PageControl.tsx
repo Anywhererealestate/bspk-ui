118c60b651b322b77905cb043d12ce9f58c424af
anybrian <brian.reed@anywhere.re>
2025-09-18 13:28:46 -0400

    fix: update @bspk/icons, test fixes

--- src/components/PageControl/PageControl.tsx
+++ src/components/PageControl/PageControl.tsx
@@ -4,21 +4,22 @@ const MAX_DOT_COUNT = 5 as const;
 
 export type PageControlProps = {
     /**
-     * The current page index (zero-based).
+     * The current page (NOT zero-based).
      *
      * @example
-     *     0;
+     *     1;
      *
+     * @min 1
      * @required
      */
-    value: number;
-
+    currentPage: number;
     /**
      * The total number of pages.
      *
      * @example
      *     5;
      *
+     * @min 1
      * @required
      */
     numPages: number;
@@ -46,20 +47,23 @@ type DotSize = 'medium' | 'small' | 'x-small';
  * @name PageControl
  * @phase UXReview
  */
-export function PageControl({ value, numPages, variant = 'flat' }: PageControlProps) {
-    if (numPages < 2) return null;
+export function PageControl({ currentPage: currentProp, numPages: numPagesProp, variant = 'flat' }: PageControlProps) {
+    const numPages = Number(numPagesProp);
+    const current = Number(currentProp);
+
+    if (numPages < 2 || current < 1 || current > numPages) return null;
 
     return (
         <span
-            aria-label={`Page ${value + 1} of ${numPages}`}
+            aria-label={`Page ${current} of ${numPages}`}
             data-bspk="page-control"
             data-variant={variant || undefined}
             role="img"
         >
-            {getDots(value, numPages).map(({ pageIndex, size }, index) => (
+            {getDots(current, numPages).map(({ pageIndex, size }, index) => (
                 <span
                     aria-hidden="true"
-                    data-active={pageIndex === Number(value) || undefined}
+                    data-active={pageIndex === current || undefined}
                     data-dot={pageIndex}
                     data-size={size}
                     key={index + 1}
@@ -70,14 +74,14 @@ export function PageControl({ value, numPages, variant = 'flat' }: PageControlPr
     );
 }
 
-function getDots(currentIndex: number, totalPages: number) {
+function getDots(currentPage: number, totalPages: number) {
     if (totalPages <= MAX_DOT_COUNT) {
         return Array.from({ length: totalPages }, (_, i) => ({
             size: 'medium' as DotSize,
             pageIndex: i,
         }));
     }
-    const start = Math.max(0, Math.min(currentIndex - 2, totalPages - MAX_DOT_COUNT));
+    const start = Math.max(0, Math.min(currentPage - 1, totalPages - MAX_DOT_COUNT));
     return Array.from({ length: MAX_DOT_COUNT }, (_, i) => {
         const pageIndex = start + i;
         let size: DotSize = 'medium';
