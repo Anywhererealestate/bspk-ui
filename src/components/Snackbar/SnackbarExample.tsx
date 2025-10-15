611a10035a581418eebec0a8762bd27f9c8e8da1
anybrian <brian.reed@anywhere.re>
2025-10-15 16:01:53 -0400

    use prop state

--- src/components/Snackbar/SnackbarExample.tsx
+++ src/components/Snackbar/SnackbarExample.tsx
@@ -1,25 +1,18 @@
-import { useState } from 'react';
 import { Snackbar, SnackbarProps } from './Snackbar';
 import { Button } from '-/components/Button';
 import { ComponentExample } from '-/utils/demo';
-
-function SnackbarDemo(props: SnackbarProps) {
-    const [snackbarOpen, setSnackbarOpen] = useState(false);
-
-    return (
-        <>
-            <Snackbar {...props} onClose={() => setSnackbarOpen(false)} open={snackbarOpen} />
-            <Button label="open snackbar" onClick={() => setSnackbarOpen(true)} size="medium" title="Snackbar" />
-        </>
-    );
-}
 
 export const SnackbarExample: ComponentExample<SnackbarProps> = {
     containerStyle: { width: '100%' },
     defaultState: {},
     disableProps: [],
     presets: [],
-    render: ({ props }) => <SnackbarDemo {...props} />,
+    render: ({ props, setState }) => (
+        <>
+            <Snackbar {...props} onClose={() => setState({ open: false })} />
+            <Button label="open snackbar" onClick={() => setState({ open: true })} size="medium" title="Snackbar" />
+        </>
+    ),
     sections: [],
-    variants: {},
+    variants: false,
 };
