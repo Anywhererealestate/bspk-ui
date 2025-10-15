3371c00954eb51b53655265a0dea9a4f3b3aabae
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-10-15 12:44:00 -0500

    chore: add an example theme

---
+++ src/components/StylesProviderExampleTheme/StylesProviderExampleTheme.tsx
@@ -0,0 +1,15 @@
+import '@bspk/styles/example-theme.css';
+import '-/styles/base.scss';
+
+/**
+ * Utility to provide the ExampleTheme styles to the application.
+ *
+ * @name StylesProviderExampleTheme
+ * @phase Utility
+ * @generated
+ */
+export function StylesProviderExampleTheme(): JSX.Element | null {
+    return null;
+}
+
+/** Copyright 2025 Anywhere Real Estate - CC BY 4.0 */
