6da9bd79ec14cb30273c2775c685494947eaa97d
anybrian <brian.reed@anywhere.re>
2025-10-20 16:43:21 -0400

    example cleanup

--- src/components/DatePicker/DatePickerExample.tsx
+++ src/components/DatePicker/DatePickerExample.tsx
@@ -1,5 +1,4 @@
 import { DatePickerProps } from '.';
-import { Field, FieldDescription, FieldLabel } from '-/components/Field';
 import { ComponentExample, Preset } from '-/utils/demo';
 
 export const presets: Preset<DatePickerProps>[] = [];
@@ -10,17 +9,6 @@ export const DatePickerExample: ComponentExample<DatePickerProps> = {
     },
     disableProps: [],
     presets,
-    render: ({ props, Component }) => {
-        const { value, ...restProps } = props;
-
-        return (
-            <Field>
-                <FieldLabel>Date</FieldLabel>
-                <Component {...restProps} value={value?.getDate ? value : undefined} />
-                <FieldDescription>The date picker allows you to select a date.</FieldDescription>
-            </Field>
-        );
-    },
     sections: [],
     variants: false,
 };
