aca945c8d2b257355e845eaf7b84b4190fd0b04e
anybrian <brian.reed@anywhere.re>
2025-10-21 15:27:07 -0400

    doc update

--- src/components/FormField/FormField.tsx
+++ src/components/FormField/FormField.tsx
@@ -5,7 +5,7 @@ export type FormFieldControlProps<P extends Record<string, unknown>> = Omit<Form
     Omit<P, keyof FormFieldProps | 'invalid'>;
 
 export type FormFieldProps = {
-    /** The error message to display when the field is invalid. */
+    /** The error message to display which when set makes the field invalid. */
     errorMessage?: string;
     /**
      * The label of the field.
