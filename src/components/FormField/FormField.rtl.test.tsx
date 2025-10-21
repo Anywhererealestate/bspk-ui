306cca853c7eec12323dba82899098d2888c8d32
anybrian <brian.reed@anywhere.re>
2025-10-21 13:01:11 -0400

    form field test

--- src/components/FormField/FormField.rtl.test.tsx
+++ src/components/FormField/FormField.rtl.test.tsx
@@ -1,17 +1,19 @@
-import { FormField } from './FormField';
-import { presets } from './FormFieldExample';
+import { FormFieldExampleRender, presets } from './FormFieldExample';
 import { hasNoBasicA11yIssues } from '-/rtl/hasNoBasicA11yIssues';
 import { render } from '-/rtl/util';
 
 describe('FormField (RTL)', () => {
     presets.forEach((preset) => {
-        it(`has no basic a11y issues - ${preset.label}`, hasNoBasicA11yIssues(<FormField {...preset.propState} />));
+        it(
+            `has no basic a11y issues - ${preset.label}`,
+            hasNoBasicA11yIssues(<FormFieldExampleRender {...preset.propState} />),
+        );
     });
 
     it('renders', () => {
-        const { getByText } = render(<FormField {...presets[0].propState} />);
+        const { getByText } = render(<FormFieldExampleRender {...presets[0].propState} />);
 
-        expect(getByText('whoop there it is')).toBeInTheDocument();
+        expect(getByText('InputPhone')).toBeInTheDocument();
     });
 });
 
--- src/components/FormField/FormFieldExample.tsx
+++ src/components/FormField/FormFieldExample.tsx
@@ -1,5 +1,5 @@
-import { ComponentType, useState } from 'react';
-import { FormFieldProps } from '.';
+import { useState } from 'react';
+import { FormField, FormFieldProps } from '.';
 import { DatePicker } from '-/components/DatePicker';
 import { FieldControlProp } from '-/components/Field';
 import { Input } from '-/components/Input';
@@ -11,7 +11,7 @@ import { Textarea } from '-/components/Textarea';
 import { TimePicker } from '-/components/TimePicker';
 import { ComponentExample, Preset } from '-/utils/demo';
 
-type ExampleProps = FieldControlProp & FormFieldProps;
+type ExampleProps = Partial<FieldControlProp & FormFieldProps>;
 
 export const presets: Preset<Partial<ExampleProps>>[] = [
     {
@@ -27,17 +27,17 @@ export const FormFieldExample: ComponentExample<ExampleProps> = {
     },
     disableProps: [],
     presets: presets as Preset<ExampleProps>[],
-    render: ({ props, Component }) => <ExampleRender Component={Component} props={props} />,
+    render: ({ props }) => <FormFieldExampleRender {...props} />,
     sections: [],
     variants: false,
 };
 
-function ExampleRender({ props, Component }: { props: ExampleProps; Component: ComponentType<FormFieldProps> }) {
+export function FormFieldExampleRender(props: ExampleProps) {
     const [value, setValue] = useState<unknown>();
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', gap: 'var(--spacing-sizing-04)' }}>
-            <Component {...props} label="DatePicker">
+            <FormField {...props} label="DatePicker">
                 <DatePicker
                     disabled={props.disabled}
                     name="input"
@@ -45,8 +45,8 @@ function ExampleRender({ props, Component }: { props: ExampleProps; Component: C
                     placeholder="Example input"
                     value={value as string}
                 />
-            </Component>
-            <Component {...props} label="Input">
+            </FormField>
+            <FormField {...props} label="Input">
                 <Input
                     disabled={props.disabled}
                     name="input"
@@ -54,17 +54,17 @@ function ExampleRender({ props, Component }: { props: ExampleProps; Component: C
                     placeholder="Example input"
                     value={value as string}
                 />
-            </Component>
-            <Component {...props} label="InputNumber">
+            </FormField>
+            <FormField {...props} label="InputNumber">
                 <InputNumber disabled={props.disabled} name="input" onChange={setValue} value={value as number} />
-            </Component>
-            <Component {...props} label="InputPhone">
+            </FormField>
+            <FormField {...props} label="InputPhone">
                 <InputPhone disabled={props.disabled} name="input" onChange={setValue} value={value as string} />
-            </Component>
-            <Component {...props} label="Password">
+            </FormField>
+            <FormField {...props} label="Password">
                 <Password disabled={props.disabled} name="input" onChange={setValue} value={value as string} />
-            </Component>
-            <Component {...props} label="Select">
+            </FormField>
+            <FormField {...props} label="Select">
                 <Select
                     disabled={props.disabled}
                     name="input"
@@ -73,8 +73,8 @@ function ExampleRender({ props, Component }: { props: ExampleProps; Component: C
                     placeholder="Example input"
                     value={value as string}
                 />
-            </Component>
-            <Component {...props} label="Textarea">
+            </FormField>
+            <FormField {...props} label="Textarea">
                 <Textarea
                     disabled={props.disabled}
                     name="input"
@@ -82,10 +82,10 @@ function ExampleRender({ props, Component }: { props: ExampleProps; Component: C
                     placeholder="Example input"
                     value={value as string}
                 />
-            </Component>
-            <Component {...props} label="TimePicker">
+            </FormField>
+            <FormField {...props} label="TimePicker">
                 <TimePicker disabled={props.disabled} name="input" onChange={setValue} value={value as string} />
-            </Component>
+            </FormField>
         </div>
     );
 }
