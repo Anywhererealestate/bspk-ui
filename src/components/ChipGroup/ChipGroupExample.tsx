837a0de4d55d7c108855ba39b9d6228aa1e52861
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-10-22 13:06:26 -0500

    chore(ChipGroup): [BSPK-191] ChipGroup example clean up

--- src/components/ChipGroup/ChipGroupExample.tsx
+++ src/components/ChipGroup/ChipGroupExample.tsx
@@ -8,176 +8,52 @@ import { SvgOpportunities } from '@bspk/icons/Opportunities';
 import { SvgSignLanguage } from '@bspk/icons/SignLanguage';
 
 import { Chip } from '-/components/Chip';
-import { ChipGroup, ChipGroupProps } from '-/components/ChipGroup';
+import { ChipGroupProps } from '-/components/ChipGroup';
 import { ComponentExampleFn, Preset } from '-/utils/demo';
 
 export const presets: Preset<ChipGroupProps>[] = [
-    { label: 'wrap', propState: { wrap: true } },
     { label: 'scroll', propState: { wrap: false } },
-    { label: 'suggestion', propState: { wrap: true } },
-    { label: 'assist', propState: { wrap: true } },
-    { label: 'input', propState: { wrap: true } },
-    { label: 'filter', propState: { wrap: true } },
+    { label: 'wrap', propState: { wrap: true } },
 ];
 
 export const ChipGroupExample: ComponentExampleFn<ChipGroupProps> = ({ action }) => ({
     presets,
-    render: ({ props, preset, Component }) => {
-        if (!preset) return null;
-
+    render: ({ props, Component }) => {
         const handleChipInputClick = () => action('Chip clicked!');
 
-        switch (preset.label) {
-            case 'wrap':
-                return (
-                    <ChipGroup {...props}>
-                        <Chip
-                            label="chip 1"
-                            leadingIcon={<SvgLightbulb />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgChevronRight />}
-                        />
-                        <Chip
-                            label="chip 2"
-                            leadingIcon={<SvgIcecream />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgChevronRight />}
-                        />
-                        <Chip
-                            label="chip 3"
-                            leadingIcon={<SvgSignLanguage />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgClose />}
-                        />
-                        <Chip
-                            label="chip 4"
-                            leadingIcon={<SvgOpportunities />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgClose />}
-                        />
-                        <Chip
-                            label="chip 5"
-                            leadingIcon={<SvgCloud />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgKeyboardArrowDown />}
-                        />
-                    </ChipGroup>
-                );
-            case 'scroll':
-                return (
-                    <ChipGroup {...props}>
-                        <Chip
-                            label="chip 1"
-                            leadingIcon={<SvgLightbulb />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgChevronRight />}
-                        />
-                        <Chip
-                            label="chip 2"
-                            leadingIcon={<SvgIcecream />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgChevronRight />}
-                        />
-                        <Chip
-                            label="chip 3"
-                            leadingIcon={<SvgSignLanguage />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgClose />}
-                        />
-                        <Chip
-                            label="chip 4"
-                            leadingIcon={<SvgOpportunities />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgClose />}
-                        />
-                        <Chip
-                            label="chip 5"
-                            leadingIcon={<SvgCloud />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgKeyboardArrowDown />}
-                        />
-                    </ChipGroup>
-                );
-            case 'suggestion':
-                return (
-                    <ChipGroup {...props}>
-                        <Chip label="suggestion 1" onClick={handleChipInputClick} />
-                        <Chip label="suggestion 2" onClick={handleChipInputClick} />
-                        <Chip label="suggestion 3" onClick={handleChipInputClick} />
-                        <Chip label="suggestion 4" onClick={handleChipInputClick} />
-                        <Chip label="suggestion 5" onClick={handleChipInputClick} />
-                    </ChipGroup>
-                );
-            case 'assist':
-                return (
-                    <ChipGroup {...props}>
-                        <Chip label="assist 1" leadingIcon={<SvgLightbulb />} onClick={handleChipInputClick} />
-                        <Chip label="assist 2" onClick={handleChipInputClick} />
-                        <Chip label="assist 3" leadingIcon={<SvgSignLanguage />} onClick={handleChipInputClick} />
-                        <Chip label="assist 4" onClick={handleChipInputClick} />
-                        <Chip label="assist 5" leadingIcon={<SvgCloud />} onClick={handleChipInputClick} />
-                    </ChipGroup>
-                );
-            case 'input':
-                return (
-                    <ChipGroup {...props}>
-                        <Chip label="input 1" onClick={handleChipInputClick} removable={false} />
-                        <Chip label="input 2" onClick={handleChipInputClick} />
-                        <Chip label="input 3" onClick={handleChipInputClick} />
-                        <Chip label="input 4" onClick={handleChipInputClick} />
-                        <Chip label="input 5" onClick={handleChipInputClick} />
-                    </ChipGroup>
-                );
-            case 'filter':
-                return (
-                    <ChipGroup {...props}>
-                        <Chip label="filter 1" onClick={handleChipInputClick} trailingIcon="SvgClose" />
-                        <Chip label="filter 2" onClick={handleChipInputClick} trailingIcon="SvgChevronRight" />
-                        <Chip label="filter 3" onClick={handleChipInputClick} trailingIcon="SvgKeyboardArrowDown" />
-                        <Chip label="filter 1" onClick={handleChipInputClick} trailingBadge={{ count: 5 }} />
-                        <Chip
-                            label="filter 1"
-                            leadingIcon={<SvgIcecream />}
-                            onClick={handleChipInputClick}
-                            trailingBadge={{ count: 7 }}
-                        />
-                    </ChipGroup>
-                );
-            default:
-                return (
-                    <Component {...props}>
-                        <Chip
-                            label="chip 1"
-                            leadingIcon={<SvgLightbulb />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgChevronRight />}
-                        />
-                        <Chip
-                            label="chip 2"
-                            leadingIcon={<SvgIcecream />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgChevronRight />}
-                        />
-                        <Chip
-                            label="chip 3"
-                            leadingIcon={<SvgSignLanguage />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgClose />}
-                        />
-                        <Chip
-                            label="chip 4"
-                            leadingIcon={<SvgOpportunities />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgClose />}
-                        />
-                        <Chip
-                            label="chip 5"
-                            leadingIcon={<SvgCloud />}
-                            onClick={handleChipInputClick}
-                            trailingIcon={<SvgKeyboardArrowDown />}
-                        />
-                    </Component>
-                );
-        }
+        return (
+            <Component {...props}>
+                <Chip
+                    label="chip 1"
+                    leadingIcon={<SvgLightbulb />}
+                    onClick={handleChipInputClick}
+                    trailingIcon={<SvgChevronRight />}
+                />
+                <Chip
+                    label="chip 2"
+                    leadingIcon={<SvgIcecream />}
+                    onClick={handleChipInputClick}
+                    trailingIcon={<SvgChevronRight />}
+                />
+                <Chip
+                    label="chip 3"
+                    leadingIcon={<SvgSignLanguage />}
+                    onClick={handleChipInputClick}
+                    trailingIcon={<SvgClose />}
+                />
+                <Chip
+                    label="chip 4"
+                    leadingIcon={<SvgOpportunities />}
+                    onClick={handleChipInputClick}
+                    trailingIcon={<SvgClose />}
+                />
+                <Chip
+                    label="chip 5"
+                    leadingIcon={<SvgCloud />}
+                    onClick={handleChipInputClick}
+                    trailingIcon={<SvgKeyboardArrowDown />}
+                />
+            </Component>
+        );
     },
 });
