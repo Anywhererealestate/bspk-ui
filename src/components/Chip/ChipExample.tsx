675a9f806368b9d30ef496b2cd124635223fc663
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-11-05 12:36:54 -0600

    chore(Chip): [BSPK-203] add Chip desigin pattern presets

--- src/components/Chip/ChipExample.tsx
+++ src/components/Chip/ChipExample.tsx
@@ -57,6 +57,41 @@ export const presets: Preset<ChipProps>[] = [
             trailingBadge: { count: 2, size: 'x-small' as BadgeItem['size'] },
         },
     },
+    {
+        label: 'Assist Chip',
+        propState: {
+            label: 'Assist Chip',
+            leadingIcon: 'Cloud',
+        },
+        designPattern:
+            'A dynamic action element that helps trigger and perform an action for the customer. A supplement option to buttons. ',
+    },
+    {
+        label: 'Filter Chip',
+        propState: {
+            label: 'Filter Chip',
+            leadingIcon: 'Cloud',
+            trailingIcon: 'KeyboardArrowDown',
+        },
+        designPattern: 'Short form descriptive words that filter out content or represent active filter setting.',
+    },
+    {
+        label: 'Input Chip',
+        propState: {
+            label: 'Input Chip',
+            leadingIcon: 'Cloud',
+            trailingIcon: 'Close',
+        },
+        designPattern:
+            'A range of short form key words or pieces of information a customer enters within multi entry field.',
+    },
+    {
+        label: 'Suggest Chip',
+        propState: {
+            label: 'Suggest Chip',
+        },
+        designPattern: 'Dynamically generated options that are suggested to the customer as responses or prompts. ',
+    },
 ];
 
 export const ChipExample: ComponentExample<ChipProps> = {
