4dfb62f85939952f409024044bbf79099e734df8
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-11-06 17:28:20 -0600

    chore(Rating): [BSPK-203] update docs

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -5,8 +5,6 @@ import { ElementType, useState } from 'react';
 export type RatingSize = 'large' | 'medium' | 'small';
 export type RatingProps = {
     /**
-     * Can only manually apply when interactive is false.
-     *
      * The value of the rating between 0 and 5.
      *
      * @minimum 0
