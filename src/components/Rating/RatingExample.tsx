4409d3e05d4ad8037eed0e6ee11c445410c49ba1
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-11-07 10:21:34 -0600

    chore(Rating): [BSPK-203] add design parrterns and update doc description for onChange

--- src/components/Rating/RatingExample.tsx
+++ src/components/Rating/RatingExample.tsx
@@ -3,19 +3,19 @@ import { ComponentExampleFn, Preset } from '-/utils/demo';
 
 export const presets: Preset<RatingProps>[] = [
     {
-        label: 'Non-interactive, No Value',
+        label: 'Read Only, No Value',
         propState: {
             onChange: undefined,
             value: undefined,
         },
     },
     {
-        label: 'Visual',
+        label: 'Read Only',
         propState: {
             onChange: undefined,
             value: 4.5,
         },
-        designPattern: 'A non interactive representation of ratings Non interactive, a scale of 1 to 5.',
+        designPattern: 'A read only representation of ratings, a scale of 1 to 5.',
     },
     {
         label: 'Interactive',
