8c9d4558b46a9ecf3809c78c299b26b36ef57642
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-11-20 13:09:19 -0600

    fix(Avatar): [BSPK-112] disabled now applies the proper styling, and no longer appears inter-actable when disabled (#286)
    * fix(Avatar): [BSPK-112] disabled now applies the proper styling, and no longer appears interactable when disabled
    
    * fix(Avatar): [BSPK-112] fix so the Tooltip does not show when disabled
    
    * fix(Avatar): [BSPK-112] allow the Tooltip to show when disabled
    
    * chore(Avatar): [BSPK-112] update example code

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -92,7 +92,6 @@ export type AvatarProps = CommonProps<'disabled'> & {
  *         image="/avatar-01.png"
  *         initials="AR"
  *         name="Andre Giant"
- *         hideTooltip
  *         size="large"
  *         disabled={false}
  *         onClick={() => action('Launch avatar popover')}
@@ -114,7 +113,7 @@ export function Avatar({
     name,
     hideTooltip = false,
     onClick,
-    disabled,
+    disabled = false,
     ...props
 }: AvatarProps) {
     const children = useMemo(() => {
