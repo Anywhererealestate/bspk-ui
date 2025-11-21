a32479c31456e682408a0c1e28f56a4f336da897
anybrian <brian.reed@anywhere.re>
2025-11-20 20:26:58 -0500

    remove month and year pickers

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,8 +1,26 @@
 import './calendar.scss';
-import { format, isValid, startOfToday } from 'date-fns';
-import { useMemo, useState } from 'react';
-import { Kind, CONFIG, useKeyDownCaptures, optionIdGenerator, HeaderButton } from './utils';
+import { SvgChevronLeft } from '@bspk/icons/ChevronLeft';
+import { SvgChevronRight } from '@bspk/icons/ChevronRight';
+import { SvgKeyboardDoubleArrowLeft } from '@bspk/icons/KeyboardDoubleArrowLeft';
+import { SvgKeyboardDoubleArrowRight } from '@bspk/icons/KeyboardDoubleArrowRight';
+import {
+    addMonths,
+    addYears,
+    eachDayOfInterval,
+    endOfMonth,
+    endOfWeek,
+    format,
+    isSameDay,
+    isValid,
+    startOfMonth,
+    startOfToday,
+    startOfWeek,
+} from 'date-fns';
+import { useEffect, useMemo, useState } from 'react';
+import { useKeyDownCaptures, optionIdGenerator } from './utils';
+import { Button } from '-/components/Button';
 import { useId } from '-/hooks/useId';
+import { getElementById } from '-/utils/dom';
 
 export type CalendarProps = {
     /**
@@ -34,66 +52,122 @@ export type CalendarProps = {
 export function Calendar({ id, value: valueProp, onChange, focusInit = false }: CalendarProps) {
     const baseId = useId(id);
 
-    const [kind, setKind] = useState<Kind>('day');
-
     const [activeDate, setActiveDate] = useState<Date>(valueProp && isValid(valueProp) ? valueProp : startOfToday());
-    const config = useMemo(() => CONFIG[kind], [kind]);
-    const rows = useMemo(() => config.rows(activeDate), [config, activeDate]);
+
+    // we don't need to recreate the rows unless the active month changes
+    const activeMonth = useMemo(() => startOfMonth(activeDate), [activeDate]);
+    const rows = useMemo(
+        () =>
+            // generate all days to be shown in the month view
+            eachDayOfInterval({
+                start: startOfWeek(activeMonth, { weekStartsOn: 0 }),
+                end: endOfWeek(endOfMonth(activeMonth), { weekStartsOn: 0 }),
+            })
+                // create groups of 7 for each week
+                .reduce<Date[][]>((row, item) => {
+                    const previousRow = row[row.length - 1];
+                    if (row.length === 0 || previousRow.length === 7) return [...row, [item]];
+                    previousRow.push(item);
+                    return row;
+                }, []),
+        [activeMonth],
+    );
 
     const { handleKeyDownCapture } = useKeyDownCaptures({
-        config,
         activeDate,
         setActiveDate,
         rows,
     });
 
-    const generateOptionId = optionIdGenerator(baseId);
+    const generateOptionId = useMemo(() => optionIdGenerator(baseId), [baseId]);
+
+    const [isDayFocused, setIsDayFocused] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (isDayFocused) getElementById(generateOptionId(activeDate))?.focus({ preventScroll: true });
+    }, [activeDate, isDayFocused, generateOptionId]);
 
     return (
         <div data-bspk="calendar" id={baseId}>
             <div data-header>
-                <HeaderButton
-                    activeDate={activeDate}
-                    direction="prev2"
-                    header={config.direction.prev2}
-                    setActiveDate={setActiveDate}
+                <Button
+                    icon={<SvgKeyboardDoubleArrowLeft />}
+                    iconOnly={true}
+                    label="Previous Year"
+                    onClick={() => setActiveDate(addYears(activeDate, -1))}
+                    size="large"
+                    variant="tertiary"
                 />
-                <HeaderButton
-                    activeDate={activeDate}
-                    direction="prev"
-                    header={config.direction.prev}
-                    setActiveDate={setActiveDate}
+                <Button
+                    icon={<SvgChevronLeft />}
+                    iconOnly={true}
+                    label="Previous Month"
+                    onClick={() => setActiveDate(addMonths(activeDate, -1))}
+                    size="large"
+                    variant="tertiary"
                 />
-                <span data-title>{config.headerLabel(activeDate, setKind)}</span>
-                <HeaderButton
-                    activeDate={activeDate}
-                    direction="next"
-                    header={config.direction.next}
-                    setActiveDate={setActiveDate}
+                <span data-title>{format(activeDate, 'MMMM yyyy')}</span>
+                <Button
+                    icon={<SvgChevronRight />}
+                    iconOnly={true}
+                    label="Next Month"
+                    onClick={() => setActiveDate(addMonths(activeDate, 1))}
+                    size="large"
+                    variant="tertiary"
                 />
-                <HeaderButton
-                    activeDate={activeDate}
-                    direction="next2"
-                    header={config.direction.next2}
-                    setActiveDate={setActiveDate}
+                <Button
+                    icon={<SvgKeyboardDoubleArrowRight />}
+                    iconOnly={true}
+                    label="Next Year"
+                    onClick={() => setActiveDate(addYears(activeDate, 1))}
+                    size="large"
+                    variant="tertiary"
                 />
             </div>
-            <table data-kind={kind} role="grid">
-                {config.tableHeader}
-                <tbody onKeyDownCapture={handleKeyDownCapture}>
+            <table role="grid">
+                <thead>
+                    <tr>
+                        <th abbr="Sunday" scope="col">
+                            Sun
+                        </th>
+                        <th abbr="Monday" scope="col">
+                            Mon
+                        </th>
+                        <th abbr="Tuesday" scope="col">
+                            Tue
+                        </th>
+                        <th abbr="Wednesday" scope="col">
+                            Wed
+                        </th>
+                        <th abbr="Thursday" scope="col">
+                            Thu
+                        </th>
+                        <th abbr="Friday" scope="col">
+                            Fri
+                        </th>
+                        <th abbr="Saturday" scope="col">
+                            Sat
+                        </th>
+                    </tr>
+                </thead>
+                <tbody
+                    onBlurCapture={() => setIsDayFocused(false)}
+                    onFocusCapture={() => setIsDayFocused(true)}
+                    onKeyDownCapture={handleKeyDownCapture}
+                >
                     {rows.map((week, weekIndex) => (
                         <tr key={weekIndex}>
                             {week.map((date) => {
-                                const label = format(date, config.optionLabelFormat);
+                                const label = format(date, 'd');
                                 const optionId = generateOptionId(date);
-                                const isActive = config.compare(date, activeDate);
+                                const isActive = isSameDay(date, activeDate);
                                 return (
                                     <td
-                                        aria-label={format(date, config.optionAriaLabelFormat)}
+                                        aria-label={format(date, 'do MMMM yyyy')}
                                         data-selected={isActive || undefined}
                                         id={optionId}
                                         key={date.toString()}
-                                        onClick={() => config.optionOnClick({ date, setActiveDate, onChange, setKind })}
+                                        onClick={() => onChange(date)}
                                         ref={(node) => {
                                             if (focusInit && isActive && node) {
                                                 setTimeout(() => {
--- src/components/Calendar/utils.tsx
+++ src/components/Calendar/utils.tsx
@@ -1,44 +1,7 @@
-import { SvgChevronLeft } from '@bspk/icons/ChevronLeft';
-import { SvgChevronRight } from '@bspk/icons/ChevronRight';
-import { SvgKeyboardDoubleArrowLeft } from '@bspk/icons/KeyboardDoubleArrowLeft';
-import { SvgKeyboardDoubleArrowRight } from '@bspk/icons/KeyboardDoubleArrowRight';
-import {
-    addDays,
-    addMonths,
-    addYears,
-    endOfMonth,
-    endOfWeek,
-    format,
-    startOfDecade,
-    startOfMonth,
-    startOfWeek,
-    isSameDay,
-    isSameMonth,
-    isSameYear,
-    setMonth,
-    endOfDecade,
-    eachDayOfInterval,
-    eachYearOfInterval,
-    parse,
-    isValid,
-} from 'date-fns';
-import { ReactNode } from 'react';
-import { Button } from '-/components/Button';
+import { addDays, addMonths, format, isSameDay, parse, isValid } from 'date-fns';
 import { handleKeyDown } from '-/utils/handleKeyDown';
 
-/**
- * |    << |  <   |    Label    |  >   |  >>   |
- * | ----: | :--: | :---------: | :--: | :---: |
- * | prev2 | prev | headerLabel | next | next2 |
- */
-export type Direction = 'next' | 'next2' | 'prev' | 'prev2';
-
-export type Kind = 'day' | 'month' | 'year';
-
-type HeaderButtonProps = {
-    label: string;
-    incrementFn: (date: Date, amount: number) => Date;
-};
+export const COLUMNS_COUNT = 7;
 
 export function parseDate(next: string | undefined) {
     // don't allow parsing unless the format is approximately mm/dd/yyyy
@@ -59,201 +22,11 @@ export function optionIdGenerator(baseId: string) {
     return (date: Date) => `${baseId}-option-${format(date, 'MM-dd-yyyy')}`;
 }
 
-export type ConfigKind = {
-    direction: {
-        [key in Direction]?: HeaderButtonProps | null;
-    };
-    columns: number;
-    headerLabel: (activeDate: Date, setKind: (next: Kind) => void) => ReactNode;
-    rows: (date: Date) => Date[][];
-    compare: (dateLeft: Date, dateRight: Date) => boolean;
-    incrementFn: (date: Date, amount: number) => Date;
-    tableHeader?: ReactNode;
-    optionLabelFormat: string;
-    optionAriaLabelFormat: string;
-    optionAttributes?: (date: Date, activeDate: Date) => Record<string, unknown>;
-    optionOnClick: (params: {
-        date: Date;
-        setActiveDate: (next: Date) => void;
-        onChange: (next: Date) => void;
-        setKind: (next: Kind) => void;
-    }) => void;
-};
-
-function createRows(dates: Date[], columns: number): Date[][] {
-    return dates.reduce<Date[][]>((row, item) => {
-        const previousRow = row[row.length - 1];
-        if (row.length === 0 || previousRow.length === columns) return [...row, [item]];
-        previousRow.push(item);
-        return row;
-    }, []);
-}
-
-/**
- * Configuration for each of the calendar picker modes (day, month, year)
- *
- * This helps keep the main component cleaner and easier to read and avoid lots of conditionals.
- */
-export const CONFIG: Record<Kind, ConfigKind> = {
-    day: {
-        direction: {
-            prev2: { label: 'Previous Year', incrementFn: addYears },
-            prev: { label: 'Previous Month', incrementFn: addMonths },
-            next: { label: 'Next Month', incrementFn: addMonths },
-            next2: { label: 'Next Year', incrementFn: addYears },
-        },
-        headerLabel: (activeDate, setKind) => (
-            <>
-                <Button
-                    aria-label={`Change Month, currently ${format(activeDate, 'MMMM')}`}
-                    data-month
-                    iconOnly
-                    label={format(activeDate, 'MMMM')}
-                    onClick={() => setKind('month')}
-                    size="large"
-                    variant="tertiary"
-                />
-                <Button
-                    aria-label={`Change Year, currently ${activeDate.getFullYear()}`}
-                    data-year
-                    iconOnly
-                    label={`${activeDate.getFullYear()}`}
-                    onClick={() => setKind('year')}
-                    size="large"
-                    variant="tertiary"
-                />
-            </>
-        ),
-        columns: 7,
-        rows: (date: Date) => {
-            return createRows(
-                eachDayOfInterval({
-                    start: startOfWeek(startOfMonth(date), { weekStartsOn: 0 }),
-                    end: endOfWeek(endOfMonth(date), { weekStartsOn: 0 }),
-                }),
-                7,
-            );
-        },
-        compare: isSameDay,
-        incrementFn: addDays,
-        tableHeader: (
-            <thead>
-                <tr>
-                    <th abbr="Sunday" scope="col">
-                        Sun
-                    </th>
-                    <th abbr="Monday" scope="col">
-                        Mon
-                    </th>
-                    <th abbr="Tuesday" scope="col">
-                        Tue
-                    </th>
-                    <th abbr="Wednesday" scope="col">
-                        Wed
-                    </th>
-                    <th abbr="Thursday" scope="col">
-                        Thu
-                    </th>
-                    <th abbr="Friday" scope="col">
-                        Fri
-                    </th>
-                    <th abbr="Saturday" scope="col">
-                        Sat
-                    </th>
-                </tr>
-            </thead>
-        ),
-        optionLabelFormat: 'd',
-        optionAriaLabelFormat: 'do MMMM yyyy',
-        optionAttributes: (date: Date, activeDate: Date) => ({
-            'data-non-month-day': date.getMonth() !== activeDate.getMonth() ? true : undefined,
-        }),
-        optionOnClick: ({ date, onChange }) => onChange(date),
-    },
-    month: {
-        direction: {
-            prev: { label: 'Previous Year', incrementFn: addYears },
-            prev2: null,
-            next2: null,
-            next: { label: 'Next Year', incrementFn: addYears },
-        },
-        headerLabel: (activeDate, setKind) => (
-            <Button
-                aria-label={`Change Year, currently ${activeDate.getFullYear()}`}
-                iconOnly
-                label={`${activeDate.getFullYear()}`}
-                onClick={() => setKind('year')}
-                size="large"
-                variant="tertiary"
-            />
-        ),
-        columns: 3,
-        rows: (date: Date) => {
-            // 12 months, 3 columns
-            return createRows(
-                Array.from({ length: 12 }, (_, i) => setMonth(date, i)),
-                3,
-            );
-        },
-        incrementFn: addMonths,
-        compare: isSameMonth,
-        optionLabelFormat: 'MMM',
-        optionAriaLabelFormat: 'MMMM yyyy',
-        optionOnClick: ({ date, setActiveDate, setKind }) => {
-            setActiveDate(date);
-            setKind('day');
-        },
-    },
-    year: {
-        direction: {
-            prev: { label: 'Earlier Years', incrementFn: (d) => addYears(d, -10) },
-            prev2: null,
-            next2: null,
-            next: { label: 'Later Years', incrementFn: (d) => addYears(d, 10) },
-        },
-        headerLabel: (activeDate, setKind) => {
-            const startYear = startOfDecade(activeDate).getFullYear();
-            const endYear = endOfDecade(activeDate).getFullYear();
-
-            return (
-                <Button
-                    aria-label={`Change Decade, currently ${startYear} to ${endYear}`}
-                    iconOnly
-                    label={`${startYear - 1} - ${endYear + 10}`}
-                    onClick={() => setKind('year')}
-                    size="large"
-                    variant="tertiary"
-                />
-            );
-        },
-        columns: 4,
-        rows: (date: Date) => {
-            return createRows(
-                eachYearOfInterval({
-                    start: addYears(startOfDecade(date), -1),
-                    end: addYears(endOfDecade(date), 1),
-                }),
-                4,
-            );
-        },
-        compare: isSameYear,
-        incrementFn: addYears,
-        optionLabelFormat: 'yyyy',
-        optionAriaLabelFormat: 'yyyy',
-        optionOnClick: ({ date, setActiveDate, setKind }) => {
-            setActiveDate(date);
-            setKind('month');
-        },
-    },
-};
-
 export const useKeyDownCaptures = ({
-    config,
     activeDate,
     setActiveDate,
     rows,
 }: {
-    config: ConfigKind;
     activeDate: Date;
     setActiveDate: (date: Date) => void;
     rows: Date[][];
@@ -267,8 +40,8 @@ export const useKeyDownCaptures = ({
         // In month mode, left/right moves one month, up/down moves 3 months
         // In year mode, left/right moves one year, up/down moves 4 years
         // This is determined by the number of columns in the grid for each mode
-        const amount = direction === 'left' || direction === 'right' ? 1 : config.columns;
-        const next = config.incrementFn(activeDate, amount * multiplier);
+        const amount = direction === 'left' || direction === 'right' ? 1 : COLUMNS_COUNT;
+        const next = addDays(activeDate, amount * multiplier);
         setActiveDate(next);
     };
 
@@ -285,25 +58,15 @@ export const useKeyDownCaptures = ({
                 ArrowLeft: handleItemArrows('left'),
                 ArrowRight: handleItemArrows('right'),
                 End: () => {
-                    const currentRow = rows.find((r) => r.some((d) => config.compare(d, activeDate)));
-                    if (!currentRow) return;
-                    const next = currentRow[currentRow.length - 1];
-                    setActiveDate(next);
+                    const next = rows.find((r) => r.some((d) => isSameDay(d, activeDate)))?.[COLUMNS_COUNT - 1];
+                    if (next) setActiveDate(next);
                 },
                 Home: () => {
-                    const currentRow = rows.find((r) => r.some((d) => config.compare(d, activeDate)));
-                    if (!currentRow) return;
-                    const next = currentRow[0];
-                    setActiveDate(next);
-                },
-                PageDown: () => {
-                    const next = config.direction.next?.incrementFn(activeDate, 1);
+                    const next = rows.find((r) => r.some((d) => isSameDay(d, activeDate)))?.[0];
                     if (next) setActiveDate(next);
                 },
-                PageUp: () => {
-                    const next = config.direction.prev?.incrementFn(activeDate, -1);
-                    if (next) setActiveDate(next);
-                },
+                PageDown: () => setActiveDate(addMonths(activeDate, 1)),
+                PageUp: () => setActiveDate(addMonths(activeDate, -1)),
                 Space: enterSpaceHandler,
                 Enter: enterSpaceHandler,
             },
@@ -314,36 +77,3 @@ export const useKeyDownCaptures = ({
         ),
     };
 };
-
-export const HEADER_DIRECTION_ICONS: Record<Direction, ReactNode> = {
-    prev: <SvgChevronLeft />,
-    prev2: <SvgKeyboardDoubleArrowLeft />,
-    next: <SvgChevronRight />,
-    next2: <SvgKeyboardDoubleArrowRight />,
-};
-
-export function HeaderButton({
-    direction,
-    header,
-    setActiveDate,
-    activeDate,
-}: {
-    direction: Direction;
-    header: ConfigKind['direction'][Direction];
-    setActiveDate: (date: Date) => void;
-    activeDate: Date;
-}) {
-    return (
-        header && (
-            <Button
-                icon={HEADER_DIRECTION_ICONS[direction]}
-                iconOnly={true}
-                key={direction}
-                label={header.label}
-                onClick={() => setActiveDate(header.incrementFn(activeDate, direction.startsWith('prev') ? -1 : 1))}
-                size="large"
-                variant="tertiary"
-            />
-        )
-    );
-}
