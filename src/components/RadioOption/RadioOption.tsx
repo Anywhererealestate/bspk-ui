aa2deccba13728cb27cd88dfe59fcf337ac8abc5
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2025-12-16 09:43:52 -0600

    chore(RadioOption): remove ToggleOption from RadioOption and use ListItem directly

--- src/components/RadioOption/RadioOption.tsx
+++ src/components/RadioOption/RadioOption.tsx
@@ -1,7 +1,15 @@
 import { RadioProps, Radio } from '-/components/Radio';
-import { ToggleOption, ToggleOptionControlProps } from '-/components/ToggleOption';
+import { ListItem } from '-/components/ListItem';
+import { ReactElement } from 'react';
 
-export type RadioOptionProps = ToggleOptionControlProps<RadioProps>;
+export type RadioOptionProps = RadioProps & {
+    /** The label of the option. Also used as the aria-label of the control. */
+    label: string;
+    /** The description of the option. */
+    description?: string;
+    /** The control element to use. */
+    children?: ReactElement;
+};
 
 /**
  * A control that allows users to choose one or more items from a list or turn an feature on or off.
@@ -12,14 +20,17 @@ export type RadioOptionProps = ToggleOptionControlProps<RadioProps>;
  * @phase Utility
  */
 export function RadioOption({ label: labelProp, description, disabled, ...radioProps }: RadioOptionProps) {
-    const label = labelProp || description;
     const ariaLabel = description ? `${labelProp} - ${description}` : labelProp;
     return (
-        label && (
-            <ToggleOption data-bspk="radio-option" description={description} disabled={disabled} label={label}>
-                <Radio {...radioProps} aria-label={ariaLabel} disabled={disabled} />
-            </ToggleOption>
-        )
+        <ListItem
+            aria-disabled={disabled || undefined}
+            as="label"
+            label={labelProp}
+            leading={<Radio {...radioProps} aria-label={ariaLabel} disabled={disabled} />}
+            owner="radio-option"
+            subText={description}
+            width="hug"
+        />
     );
 }
 
