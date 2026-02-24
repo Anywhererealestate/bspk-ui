5adab0ebb2df9de6f50e4de3c7dabaf37e8722d1
Jessica McIntosh <jessica.mcintosh@anywhere.re>
2026-02-24 08:15:05 -0600

    fix(OTPInput): [BSPK-473] remove aria-labelledby and apply cursor: not-allowed… (#349)
    * fix(OTPInput): [BSPK-473] remove aria-labelledby and apply cursor: not-allowed style to inputs that cannot be selected directly, last input stay on input if deleted like the others
    
    * chore(OTPInput): [BSPK-473] comment clean up

--- src/components/OTPInput/OTPInput.tsx
+++ src/components/OTPInput/OTPInput.tsx
@@ -129,21 +129,26 @@ export function OTPInput({
     };
 
     const Backspace = (digitIndex: number) => () => {
-        // if we are editing an input before the last one and it has a value, just clear the value and stay on that input
-        if (inputs[digitIndex + 1]?.value) {
+        // If the current input has a value, just clear it and stay on this input
+        if (inputs[digitIndex]?.value) {
             setValues((prev) => {
                 const newValues = [...prev];
                 newValues[digitIndex] = '';
                 return newValues;
             });
+            inputs[digitIndex]?.focus();
             return;
         }
 
-        setValues((prev) => {
-            return prev.filter((_, index) => index !== digitIndex);
-        });
-
-        inputs[Math.max(0, digitIndex - 1)]?.focus();
+        // If not the first input, move focus to the previous input
+        if (digitIndex > 0) {
+            setValues((prev) => {
+                const newValues = [...prev];
+                newValues[digitIndex - 1] = '';
+                return newValues;
+            });
+            inputs[digitIndex - 1]?.focus();
+        }
     };
 
     const canBeFocused = (index: number) => {
@@ -173,7 +178,7 @@ export function OTPInput({
 
     return (
         <div
-            aria-labelledby={`${id}-label`}
+            aria-label={ariaLabel}
             data-bspk="otp-input"
             data-disabled={disabled || undefined}
             data-invalid={invalid || undefined}
@@ -206,6 +211,7 @@ export function OTPInput({
                         autoComplete="off"
                         data-index={index}
                         data-main-input={true}
+                        data-not-selectable={canBeFocused(index) ? undefined : true}
                         disabled={disabled || undefined}
                         inputMode={alphanumeric ? 'text' : 'numeric'}
                         maxLength={1}
@@ -245,6 +251,10 @@ export function OTPInput({
                             )(event);
                         }}
                         onMouseDown={(event) => {
+                            if (!canBeFocused(index)) {
+                                inputs[values.length]?.focus();
+                            }
+
                             if (canBeFocused(index)) return;
 
                             event.preventDefault();
